7d150700c04be12f64b09607c9394d2265b8f1a8
Calorab <calebwgammon@gmail.com>
2024-05-01 17:03:22 -0400

    security updates

--- get_data.py
+++ get_data.py
@@ -1,42 +1,43 @@
 import os
 import snowflake.connector
-import pandas as pd
+import logging
+from dotenv import load_dotenv
 
-'''Todo:
-    1: rebuild county views using UNION so that I get one table for all counties
-    2: rebuild Views Task in Snowflake
-'''
+logging.basicConfig(filename='snowflake_data.log', filemode='w', level=logging.INFO, format='%(asctime)s - %(message)s')
+load_dotenv()
+
+sf_password = os.getenv('SNOWFLAKE_PASSWORD')
+sf_user = os.getenv('SNOWFLAKE_USER')
+sf_account = os.getenv('SNOWFLAKE_ACCOUNT')
 
 # Set up connection to Snowflake
 conn = snowflake.connector.connect(
-    user='ETLPROG2023',
-    password='NHqLFb2X6#CgoLtn',
-    account='omb53008.us-east-1'
+    user= sf_user,
+    password= sf_password,
+    account= sf_account
 )
 
-# Create a the snowflake cursor object
+# Create the snowflake cursor object
 cur = conn.cursor()
-# 
 tables = ('DEMOGRAPHICS', 'HOUSING', 'PARKS_ADDRESS_API_DATA', 'PARKS_API_DATA', 'POI_INDEX_COMP', 'WEATHER_CLIMATE')
 
+# Apply Role and Warehouse to queries
 cur.execute('USE ROLE COL_ADMIN;')
 cur.execute('USE DATABASE COLORADO;')
 cur.execute('USE SCHEMA ANALYTICS;')
 cur.execute('USE WAREHOUSE COL_WH;')
 
-
-print(f'Before response from COUNTIES')
 # execute statement
 df = cur.execute('SELECT * FROM COLORADO.ANALYTICS.COUNTIES;').fetch_pandas_all()
-print('Got the response')
+logging.info('Got the response from Counties table')
 
-# open dataFrame and write to a file data/<filename> probably just the table name
+# open dataFrame and write to a file data/<tablename> 
 if not os.path.exists('data'):
     os.mkdir('data')
-    print('Data directory created')
+    logging.info('Data directory created')
 
 with open(os.path.join('data', 'counties'), 'w') as f:
-    # write flat file data to this file
+    # write flat file data to counties file
     df.to_csv(f, header=True, index=False)
 
     
@@ -46,23 +47,23 @@ for table in tables:
     db_table = 'COLORADO.ANALYTICS.' + table
     query = f'SELECT * FROM {db_table};'
     location = os.path.join('data', file_name)
-    print(f'Before response from {table}')
+    
     # execute statement
     df = cur.execute(query).fetch_pandas_all()
     # open dataFrame and write to a file data/<filename> probably just the table name
-    print('Got the response')
+    logging.info(f'Got the response from {table}')
 
     if not os.path.exists('data'):
         os.mkdir('data')
-        print('Data directory created')
+        logging.info('Data directory created')
 
     
     with open(location, 'w') as f:
         # write flat file data to this file
         df.to_csv(f, header=True, index=False)
 
-
-
+logging.info('get_data.py completed successfully')
+logging.info('Closing Snowflake connection')
 cur.close()
 
 
